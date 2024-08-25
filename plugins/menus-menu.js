9881a3645544254bf52c11537648ee329efdcc1e
Xnnv2 <toscren0@gmail.com>
2024-08-24 23:11:19 -0500

    XDD

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -1,30 +1,19 @@
 import fetch from 'node-fetch';
-
-// Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-// Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-// To set the language, in the root of the project, modify the config.json file.
-
-const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
-
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
-    const datas = global
-    const idioma = datas.db.data.users[m.sender].language
-    const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-    const tradutor = _translate.plugins.menu_menu
-    // const pp = imagen7;
-
+    const pp = imagen4;
     // let vn = './media/menu.mp3'
     const img = './Menu2.jpg';
     const d = new Date(new Date + 3600000);
     const locale = 'es-ES';
-    const week = d.toLocaleDateString(locale, { weekday: 'long' });
-    const date = d.toLocaleDateString(locale, { day: '2-digit', month: '2-digit', year: 'numeric' });
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: '2-digit', month: '2-digit', year: 'numeric'});
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const user = global.db.data.users[m.sender];
-    const { money, joincount } = global.db.data.users[m.sender];
-    const { exp, limit, level, role } = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
     const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
     const rtotal = Object.entries(global.db.data.users).length || '0'
     const more = String.fromCharCode(8206);
@@ -32,20 +21,20 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `┎━━━━━━━ ∙ʚ🤴🏻ɞ∙ ━━━━━━━┒
-┃⭐ Creador : 𝐐𝐮𝐞𝐬𝐨 𝐁𝐨𝐭
-┃📞 Wa.me/573009684519
-┃https://chat.whatsapp.com/Hk5iEubNN581Rv2rH4vSm0
-┖━━━━━━━ ∙ʚ⚜️ɞ∙ ━━━━━━━┚
+    const str = `*`⌜ ＭＥＮＵ | ＣＯＭＰＬＥＴＯ ⌟`*
 
+*`╭━〔 𝐆𝐇𝐎𝐒𝐓 𝐁𝐎𝐓 〕━╮`*
+┃➔ ⭐ *Owner:* Alex
+┃➔ 📱 *Contacto*: wa.me/595984303784
+┃➔ 💌 *Pruebas:* https://chat.whatsapp.com/IGsGRrP8paEAD2mHVF0xUT
+╰━━━━━━━━━━━━━━╯
 
+*`╭━〔 𝐀𝐂𝐓𝐈𝐕𝐀𝐑|𝐃𝐄𝐒𝐀𝐂𝐓𝐈𝐕𝐀𝐑 〕━╮`*
+┃➔ ✅ ${usedPrefix}enable
+┃➔ ❌ ${usedPrefix}disable*
+╰━━━━━━━━━━━━━━╯
 
- ╭━〔 ⚙️ 𝐀𝐂𝐓𝐈𝐕𝐀𝐑 𝐎 𝐃𝐄𝐒𝐀𝐂𝐓𝐈𝐕𝐀𝐑 ⚙️ 〕
- *┃➤ 🟢 ${usedPrefix}enable*
- *┃➤ 🔴 ${usedPrefix}disable*
-  ╰━━━━━━━━━━━━
-
- ╭━〔 ❄️ 𝐆𝐑𝐔𝐏𝐎𝐒 ❄️〕
+           〔 𝐆𝐑𝐔𝐏𝐎𝐒 ❄️〕
  *┃➤  ${usedPrefix}menuff*
  *┃➤  ${usedPrefix}comandosff*
  *┃➤  ${usedPrefix}kick*
@@ -62,7 +51,7 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
  *┃➤  ${usedPrefix}kickfantasmas*
   ╰━━━━━━━━━━━━
 
- ╭━〔 ⚠️ *FREE FIRE* ⚠️ 〕
+ ╭━〔 *FREE FIRE*🔫〕
  *┃➤ 🗺️ ${usedPrefix}menuff*
  *┃➤ 🗺️ ${usedPrefix}bermuda*
  *┃➤ 🗺️ ${usedPrefix}kalahari*
@@ -72,7 +61,7 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
  *┃➤ 📋 ${usedPrefix}agendasemanal*
   ╰━━━━━━━━━━━━
 
-  ╭━〔 ⚔️ *BUSCAR VS PLANTILLAS* 🔱 〕
+  ╭━〔 *BUSCAR VS PLANTILLAS*💌 〕
  *┃➤ 🌀 ${usedPrefix}buscarvsmasc*
  *┃➤ ⚔️ ${usedPrefix}bsmsac*
  *┃➤ ⚓ ${usedPrefix}buscarvsfem*
@@ -81,30 +70,30 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
  *┃➤ 👁️‍🗨️ ${usedPrefix}bsmixto*
   ╰━━━━━━━━━━━━
 
- *╔══ ≪ REGLAS , LISTA , FICHA ≫ ══╗*
-*║* 💻➺ _${usedPrefix}Fem4_
-*║* 💻➺ _${usedPrefix}Fem6_
-*║* 💻➺ _${usedPrefix}FemInterna4_
-*║* 💻➺ _${usedPrefix}FemInterna6_
-*║* 💻➺ _${usedPrefix}Masc4_
-*║* 💻➺ _${usedPrefix}Masc6_
-*║* 💻➺ _${usedPrefix}MascInterna4_
-*║* 💻➺ _${usedPrefix}MascInterna6_
-*║* 💻➺ _${usedPrefix}Mixto4_
-*║* 💻➺ _${usedPrefix}Mixto6_
-*║* 💻➺ _${usedPrefix}MixtoInterna4_
-*║* 💻➺ _${usedPrefix}MixtoInterna6_
-*║* 💻➺ _${usedPrefix}apos_
-*║* 💻➺ _${usedPrefix}mapa_
-*║* 💻➺ _${usedPrefix}Ficha / Formulario Reclutamiento_
-*║* 💻➺ _${usedPrefix}Nexterra_
-*║* 💻➺ _${usedPrefix}Bermuda_
-*║* 💻➺ _${usedPrefix}Kalahary_
-*║* 💻➺ _${usedPrefix}Alpes_
-*║* 💻➺ _${usedPrefix}AgendaSemanal_
-*╚══ ≪ •❈• ≫ ══╝*
+*╭━〔REGLAS Y LISTAS VS💙〕*
+*║* 📝➺ _${usedPrefix}Fem4_
+*║* 📝➺ _${usedPrefix}Fem6_
+*║* 📝➺ _${usedPrefix}FemInterna4_
+*║* 📝➺ _${usedPrefix}FemInterna6_
+*║* 🗒️➺ _${usedPrefix}Masc4_
+*║* 🗒️➺ _${usedPrefix}Masc6_
+*║* 🗒️➺ _${usedPrefix}MascInterna4_
+*║* 🗒️➺ _${usedPrefix}MascInterna6_
+*║* 🗒️➺ _${usedPrefix}Mixto4_
+*║* 📋➺ _${usedPrefix}Mixto6_
+*║* 📋➺ _${usedPrefix}MixtoInterna4_
+*║* 📋➺ _${usedPrefix}MixtoInterna6_
+*║* 📋➺ _${usedPrefix}apos_
+*║* 📋➺ _${usedPrefix}mapa_
+*║* 📝➺ _${usedPrefix}Ficha / Formulario Reclutamiento_
+*║* 🗽➺ _${usedPrefix}Nexterra_
+*║* 🗼➺ _${usedPrefix}Bermuda_
+*║* 🏖️➺ _${usedPrefix}Kalahary_
+*║* 🏜️➺ _${usedPrefix}Alpes_
+*║* 🏝️➺ _${usedPrefix}AgendaSemanal_
+╰━━━━━━━━━━━━
 
- *╔══ ≪ LINKS DE COMUS/APK Y DRIVES DZN ≫ ══╗*
+*╭━〔LINKS - DRIVE DZN 💙〕*
  *║* 🎐_${usedPrefix}SpamApk / WhatsApp_
  *║* 🎐_${usedPrefix}SpamComu / Comunidades_
  *║* 🎐 _${usedPrefix}Spam2_
@@ -113,7 +102,7 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
  *║* 🎐 _${usedPrefix}dzn_
  *║* 🎐 _${usedPrefix}ApkEdits_
  *║* 🎐 _${usedPrefix}DriveEdits_
- *╚══ ≪ •❈• ≫ ══╝*
+╰━━━━━━━━━━━━
 
  ╭━〔 🧿 𝐆𝐄𝐍𝐄𝐑𝐀 𝐏𝐋𝐀𝐓𝐀𝐅𝐎𝐑𝐌𝐀𝐒 🧿 〕
  *┃➤ 💴 ${usedPrefix}plataformas*
@@ -125,87 +114,80 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
  *┃➤ 💸 ${usedPrefix}cursoplataformas*
   ╰━━━━━━━━━━━
 
- *╔══ ≪ STICKERS X FILTROS ≫ ══╗*
- *║* 🎐 _${usedPrefix}sticker | s *imagen o video*_
- *║* 🎐 _${usedPrefix}sticker | s *url de tipo jpg*_
- *║* 🎐 _${usedPrefix}emojimix *😺+😆*_
- *║* 🎐 _${usedPrefix}qc*_
- *║* 🎐 _${usedPrefix}scircle | círculo *imagen*_
- *║* 🎐 _${usedPrefix}semoji | emoji *tipo emoji*_
- *║* 🎐 _${usedPrefix}attp *texto*_
- *║* 🎐 _${usedPrefix}attp2 *texto*_
- *║* 🎐 _${usedPrefix}ttp *texto*_
- *║* 🎐 _${usedPrefix}ttp2 *texto*_
- *║* 🎐 _${usedPrefix}ttp3 *texto*_
- *║* 🎐 _${usedPrefix}ttp4 *texto*_
- *║* 🎐 _${usedPrefix}ttp5 *texto*_
- *║* 🎐 _${usedPrefix}ttp6 *texto*_
- *║* 🎐 _${usedPrefix}dado_
- *║* 🎐 _${usedPrefix}stickermarker *efecto : responder a imagen*_
- *║* 🎐 _${usedPrefix}stickerfilter *efecto : responder a imagen*_
- *║* 🎐 _${usedPrefix}cs *:* cs2_
- *╚══ ≪ •❈• ≫ ══╝*
-
+*╭━〔 STICKERS ❄️〕*
+ *║* 🪐 _${usedPrefix}sticker | s *imagen o video*_
+ *║* 🪐 _${usedPrefix}sticker | s *url de tipo jpg*_
+ *║* 🪐 _${usedPrefix}emojimix *😺+😆*_
+ *║* 🪐 _${usedPrefix}qc*_
+ *║* 🪐 _${usedPrefix}scircle | círculo *imagen*_
+ *║* 🪐 _${usedPrefix}semoji | emoji *tipo emoji*_
+ *║* 🪐 _${usedPrefix}attp *texto*_
+ *║* 🪐 _${usedPrefix}attp2 *texto*_
+ *║* 🪐 _${usedPrefix}ttp *texto*_
+ *║* 🪐 _${usedPrefix}ttp2 *texto*_
+ *║* 🪐 _${usedPrefix}ttp3 *texto*_
+ *║* 🪐 _${usedPrefix}ttp4 *texto*_
+ *║* 🪐 _${usedPrefix}ttp5 *texto*_
+ *║* 🪐 _${usedPrefix}ttp6 *texto*_
+ *║* 🪐 _${usedPrefix}dado_
+ *║* 🪐 _${usedPrefix}stickermarker *efecto : responder a imagen*_
+ *║* 🪐 _${usedPrefix}stickerfilter *efecto : responder a imagen*_
+ *║* 🪐 _${usedPrefix}cs *:* cs2_
+ ╰━━━━━━━━━━━━
 
  ╭━〔 🎮 𝐉𝐔𝐄𝐆𝐎𝐒 🎮 〕
-*┃➤ 🕹️ ${usedPrefix}mates*
-*┃➤ 🕹️ ${usedPrefix}ppt*
-*┃➤ 🕹️ ${usedPrefix}prostituto <@tag>*
-*┃➤ 🕹️ ${usedPrefix}prostituta <@tag>*
-*┃➤ 🕹️ ${usedPrefix}gay2 <@tag>*
-*┃➤ 🕹️ ${usedPrefix}lesbiana <@tag>*
-*┃➤ 🕹️ ${usedPrefix}pajero <@tag>*
-*┃➤ 🕹️ ${usedPrefix}pajera <@tag>*
-*┃➤ 🕹️ ${usedPrefix}puto <@tag>*
-*┃➤ 🕹️ ${usedPrefix}puta <@tag>*
-*┃➤ 🕹️ ${usedPrefix}manco <@tag>*
-*┃➤ 🕹️ ${usedPrefix}manca <@tag>*
-*┃➤ 🕹️ ${usedPrefix}rata <@tag>*
-*┃➤ 🕹️ ${usedPrefix}negro <@tag>*
-*┃➤ 🕹️ ${usedPrefix}negra <@tag>*
-*┃➤ 🕹️ ${usedPrefix}fea <@tag>*
-*┃➤ 🕹️ ${usedPrefix}feo <@tag>*
-*┃➤ 🕹️ ${usedPrefix}sinpoto <@tag>*
-*┃➤ 🕹️ ${usedPrefix}sintetas <@tag>*
-*┃➤ 🕹️ ${usedPrefix}sinpito <@tag>*
-*┃➤ 🕹️ ${usedPrefix}adoptada <@tag>*
-*┃➤ 🕹️ ${usedPrefix}adoptado <@tag>*
-*┃➤ 🕹️ ${usedPrefix}love*
-*┃➤ 🕹️ ${usedPrefix}los10*
-*┃➤ 🕹️ ${usedPrefix}suitpvp*
-*┃➤ 🕹️ ${usedPrefix}slot*
-*┃➤ 🕹️ ${usedPrefix}simisimi*
-*┃➤ 🕹️ ${usedPrefix}pregunta*
-*┃➤ 🕹️ ${usedPrefix}cuando*
-*┃➤ 🕹️ ${usedPrefix}ship5*
-*┃➤ 🕹️ ${usedPrefix}abrazo*
-*┃➤ 🕹️ ${usedPrefix}ship2*
-*┃➤ 🕹️ ${usedPrefix}formarpareja*
-*┃➤ 🕹️ ${usedPrefix}verdad*
-*┃➤ 🕹️ ${usedPrefix}reto*
-*┃➤ 🕹️ ${usedPrefix}cancion*
-*┃➤ 🕹️ ${usedPrefix}pista*
-*┃➤ 🕹️ ${usedPrefix}ruleta*
-*┃➤ 🕹️ ${usedPrefix}zodiac*
-*┃➤ 🕹️ ${usedPrefix}odio*
-*┃➤ 🕹️ ${usedPrefix}ship*
-*┃➤ 🕹️ ${usedPrefix}sorteo*
-*┃➤ 🕹️ ${usedPrefix}minovia*
-*┃➤ 🕹️ ${usedPrefix}minovio*
-*┃➤ 🕹️ ${usedPrefix}kchero*
-*┃➤ 🕹️ ${usedPrefix}kchero*
+*┃➤ 🍁 ${usedPrefix}mates*
+*┃➤ 🍁 ${usedPrefix}ppt*
+*┃➤ 🍁 ${usedPrefix}prostituto <@tag>*
+*┃➤ 🍁 ${usedPrefix}prostituta <@tag>*
+*┃➤ 🍁 ${usedPrefix}gay2 <@tag>*
+*┃➤ 🍁 ${usedPrefix}lesbiana <@tag>*
+*┃➤ 🍁 ${usedPrefix}pajero <@tag>*
+*┃➤ 🍁 ${usedPrefix}pajera <@tag>*
+*┃➤ 🍁 ${usedPrefix}puto <@tag>*
+*┃➤ 🍁 ${usedPrefix}puta <@tag>*
+*┃➤ 🍁 ${usedPrefix}manco <@tag>*
+*┃➤ 🍁 ${usedPrefix}manca <@tag>*
+*┃➤ 🍁 ${usedPrefix}rata <@tag>*
+*┃➤ 🍁 ${usedPrefix}negro <@tag>*
+*┃➤ 🍁 ${usedPrefix}negra <@tag>*
+*┃➤ 🍁 ${usedPrefix}fea <@tag>*
+*┃➤ 🍁 ${usedPrefix}feo <@tag>*
+*┃➤ 🍁 ${usedPrefix}sinpoto <@tag>*
+*┃➤ 🍁 ${usedPrefix}sintetas <@tag>*
+*┃➤ 🍁 ${usedPrefix}sinpito <@tag>*
+*┃➤ 🍁 ${usedPrefix}adoptada <@tag>*
+*┃➤ 🍁 ${usedPrefix}adoptado <@tag>*
+*┃➤ 🍁 ${usedPrefix}love*
+*┃➤ 🍁 ${usedPrefix}suitpvp*
+*┃➤ 🍁 ${usedPrefix}slot*
+*┃➤ 🍁 ${usedPrefix}simisimi*
+*┃➤ 🍁 ${usedPrefix}pregunta*
+*┃➤ 🍁 ${usedPrefix}cuando*
+*┃➤ 🍁 ${usedPrefix}ship5*
+*┃➤ 🍁 ${usedPrefix}abrazo*
+*┃➤ 🍁 ${usedPrefix}ship2*
+*┃➤ 🍁 ${usedPrefix}formarpareja*
+*┃➤ 🍁 ${usedPrefix}verdad*
+*┃➤ 🍁 ${usedPrefix}reto*
+*┃➤ 🍁 ${usedPrefix}cancion*
+*┃➤ 🍁 ${usedPrefix}pista*
+*┃➤ 🍁 ${usedPrefix}ruleta*
+*┃➤ 🍁 ${usedPrefix}odio*
+*┃➤ 🍁 ${usedPrefix}ship*
+*┃➤ 🍁 ${usedPrefix}sorteo*
+*┃➤ 🍁 ${usedPrefix}minovia*
+*┃➤ 🍁 ${usedPrefix}minovio*
+*┃➤ 🍁 ${usedPrefix}kchero*
  ╰━━━━━━━━━━━━
 
 
  ╭━〔 🎭 *IMAGENES* 🎭 〕
-*┃➤ 🖍️ ${usedPrefix}goku*
-*┃➤ 🖍️ ${usedPrefix}vegeta*
-*┃➤ 🖍️ ${usedPrefix}lora*
-*┃➤ 🖍️ ${usedPrefix}cr7*
-*┃➤ 🖍️ ${usedPrefix}spiderman*
-*┃➤ 🖍️ ${usedPrefix}batman*
-*┃➤ 🖍️ ${usedPrefix}hellokitty*
-*┃➤ 🖍️ ${usedPrefix}charmander*
+*┃➤ 📷 ${usedPrefix}goku*
+*┃➤ 📷 ${usedPrefix}vegeta*
+*┃➤ 📷 ${usedPrefix}cr7*
+*┃➤ 📷 ${usedPrefix}spiderman*
+*┃➤ 📷 ${usedPrefix}Messi*
  ╰━━━━━━━━━━━━
  
 
@@ -240,8 +222,7 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
 *┃➤ 📍 ${usedPrefix}tts*
  ╰━━━━━━━━━━━━
 
-*╭━〔 ♓ 𝐄𝐅𝐄𝐂𝐓𝐎𝐒 𝐘 𝐋𝐎𝐆𝐎𝐒 ♓ 〕*
-*┃➤ ❄️ ${usedPrefix}mensajefalso*
+*╭━〔 ♓ 𝐄𝐅𝐄𝐂𝐓𝐎𝐒 𝐘 𝐋𝐎𝐆𝐎𝐒*
 *┃➤ ❄️ ${usedPrefix}logos*
 *┃➤ ❄️ ${usedPrefix}logocorazon*
 *┃➤ ❄️ ${usedPrefix}ytcomment*
@@ -251,9 +232,9 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
  ╰━━━━━━━━━━━━
 
 *╭━〔 💌 𝐅𝐑𝐀𝐒𝐄𝐒 𝐘 𝐓𝐄𝐗𝐓𝐎𝐒 💌〕*
-*┃➤ 🥀 ${usedPrefix}piropo*
-*┃➤ 🥀 ${usedPrefix}consejo*
-*┃➤ 🥀 ${usedPrefix}fraseromantica*
+*┃➤ 🌸 ${usedPrefix}piropo*
+*┃➤ 🌸 ${usedPrefix}consejo*
+*┃➤ 🌸 ${usedPrefix}fraseromantica*
  ╰━━━━━━━━━━━━
 
  ╭━〔 ✨ 𝐁𝐔𝐒𝐂𝐀𝐃𝐎𝐑𝐄𝐒 ✨  〕*
@@ -279,20 +260,19 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
  ╰━━━━━━━━━━━━
 
 *╭━〔 💎 𝐄𝐂𝐎𝐍𝐎𝐌𝐈𝐀𝐒 💎 〕*
-*┃➤ 🍁 ${usedPrefix}verificar*
-*┃➤ 🍁 ${usedPrefix}unreg*
-*┃➤ 🍁 ${usedPrefix}minar*
-*┃➤ 🍁 ${usedPrefix}buy*
-*┃➤ 🍁 ${usedPrefix}work*
-*┃➤ 🍁 ${usedPrefix}mendigar*
-*┃➤ 🍁 ${usedPrefix}transfer*
+*┃➤ 🍂 ${usedPrefix}verificar*
+*┃➤ 🍂 ${usedPrefix}unreg*
+*┃➤ 🍂 ${usedPrefix}minar*
+*┃➤ 🍂 ${usedPrefix}buy*
+*┃➤ 🍂 ${usedPrefix}work*
+*┃➤ 🍂 ${usedPrefix}mendigar*
+*┃➤ 🍂 ${usedPrefix}transfer*
  ╰━━━━━━━━━━━━
  *╔══ ≪ FUNCIONES OWNERS ≫ ══╗*
 *║* 💎➺ _${usedPrefix}join *enlace*_
 *║* 💎➺ _${usedPrefix}unete *enlace*_
 *║* 💎➺ _${usedPrefix}dardiamantes *cantidad*_
 *║* 💎➺ _${usedPrefix}darxp *cantidad*_
-*║* 💎➺ _${usedPrefix}dargatacoins *cantidad*_
 *║* 💎➺ _${usedPrefix}addprem | userpremium *@tag* *cantidad*_
 *║* 💎➺ _${usedPrefix}addprem2 | userpremium2 *@tag* *cantidad*_
 *║* 💎➺ _${usedPrefix}addprem3 | userpremium3 *@tag* *cantidad*_
@@ -316,42 +296,18 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
 *║* 💎➺ _${usedPrefix}añadirdiamantes *@tag cantidad*_
 *║* 💎➺ _${usedPrefix}añadirxp *@tag cantidad*_
 *║* 💎➺ _${usedPrefix}añadircoins *@tag cantidad*_
-*╚══ ≪ •❈• ≫ ══╝*`
-
-    let pp
-    // Nouvelles images de menu disponibles 
-    if (idioma == 'es') {
-      pp = global.imagen4
-    } else if (idioma == 'pt-br') {
-      pp = global.imagen7
-    } else if (idioma == 'fr') {
-      pp = global.imagen8
-    }else if (idioma == 'en') {
-      pp = global.imagen9
-    } else if (idioma == 'ru') {
-      pp = global.imagen10
-    } else {
-      pp = global.imagen4 // Imagem Default em espanhol
-    }
-
-
-
+*╚══ ≪ •❈• ≫ ══╝*`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
-const fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-     conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net') }, { quoted: fkontak });
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
     } else {
-      //await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
-const fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-   conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net') }, { quoted: fkontak });
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch {
-    const datas = global
-    const idioma = datas.db.data.users[m.sender].language
-    const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-    const tradutor = _translate.plugins.menu_menu
-
-    conn.reply(m.chat, tradutor.texto1[29], m);
+    conn.reply(m.chat, '*[ ℹ️ ] Este menu tiene un error interno, por lo cual no fue posible enviarlo.*', m);
   }
 };
 handler.command = /^(menu|menú|memu|memú|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos|cmd)$/i;
@@ -363,4 +319,4 @@ function clockString(ms) {
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
   return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
-      }
+}
